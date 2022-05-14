475d7fc0f0a4ddcc7f0742ba429c056b372aa8a3
Abimael Denis <abimael-denis@outlook.com>
2022-05-14 09:08:10 -0300

    Start Project

---
+++ src/test/java/com/abi/ala/AlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abi.ala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
